6f8f91ab181dccf0db420f1cc767152360024d2e
vibhu/1805 <vibhutisharma483@gmail.com>
2023-09-18 21:36:14 +0530

    Updated

---
+++ src/components/landing-page/Serviceinfo.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import "../../Styles/Serviceinfo.css"
+
+function Serviceinfo() {
+  return (
+    <div>
+        <div class="content-info">
+        <div class="container">
+          <div class="p-5 text-center ">
+            <h2>Create documents Quickly and Easily</h2>
+            <div class="wrapper">
+              <div class="single-card">
+                <div class="img-area">
+                  <img src="../Assets/card.jpeg" alt="" />
+                </div>
+
+                <div class="info">
+                  <h3>Affidavit For Year Gap</h3>
+                </div>
+              </div>
+
+              <div class="single-card">
+                <div class="img-area">
+                  <img src="../Assets/card.jpeg" alt="" />
+                </div>
+                <div class="info">
+                  <h3>Affidavit For Change of Name</h3>
+                </div>
+              </div>
+
+              <div class="single-card">
+                <div class="img-area">
+                  <img src="../Assets/card.jpeg" alt="" />
+                </div>
+                <div class="info">
+                  <h3>Divorce Petition</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+      
+    </div>
+  )
+}
+
+export default Serviceinfo
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import Navbar from '../components/Navbar'
 import Herosection from '../components/landing-page/Herosection'
 import Bestpart from '../components/landing-page/Bestpart'
 import ServicesSlider from '../components/landing-page/ServicesSlider'
+import Jumbo from '../components/landing-page/Jumbo'
+import Questions from '../components/landing-page/Questions'
+import Serviceinfo from '../components/landing-page/Serviceinfo'
 // import ProviderSlider from '../components/landing-page/ProviderSlider'
 
 const Home = () => {
@@ -10,8 +13,11 @@ const Home = () => {
     <div>
       <Navbar/>
       <Herosection/>
+      <Jumbo />
       <Bestpart/>
+      <Serviceinfo />
       <ServicesSlider/>
+      <Questions />
      
     </div>
   )
