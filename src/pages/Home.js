24ec2cefee6857ce44c1d99f810e13793bbc866f
vibhu/1805 <vibhutisharma483@gmail.com>
2023-09-18 22:18:45 +0530

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import Navbar from '../components/Navbar'
 import Herosection from '../components/landing-page/Herosection'
 import Bestpart from '../components/landing-page/Bestpart'
 import ServicesSlider from '../components/landing-page/ServicesSlider'
+import Jumbo from '../components/landing-page/Jumbo'
+import Questions from '../components/landing-page/Questions'
+import Serviceinfo from '../components/landing-page/Serviceinfo'
 
 // import ProviderSlider from '../components/landing-page/ProviderSlider'
 
@@ -10,11 +13,15 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
+
       <Herosection/>
+      <Jumbo />
       
       <Bestpart/>
+      <Serviceinfo />
       
       <ServicesSlider/>
+      <Questions />
       
      
     </div>
