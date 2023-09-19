762d3560919b64648193322008f589c40f2d6564
vivek sagar <vivek54540@gmail.com>
2023-09-19 17:09:34 +0530

    user feedback added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,21 @@ import Herosection from '../components/landing-page/Herosection'
 import Bestpart from '../components/landing-page/Bestpart'
 import ServicesSlider from '../components/landing-page/ServicesSlider'
 import Loginonpage from '../components/landing-page/Loginonpage'
+import ProviderSlider from '../components/landing-page/ProviderSlider'
+import StateService from '../components/landing-page/StateService'
+import Userfeed from '../components/landing-page/Userfeed'
+import Footer from '../components/Footer'
+
+
+
+
+
 
 import Jumbo from '../components/landing-page/Jumbo'
 import Questions from '../components/landing-page/Questions'
 // import Serviceinfo from '../components/landing-page/Serviceinfo'
 
 
-import ProviderSlider from '../components/landing-page/ProviderSlider'
-import StateService from '../components/landing-page/StateService'
-import Footer from '../components/Footer'
 
 
 const Home = () => {
@@ -27,6 +33,7 @@ const Home = () => {
       <ServicesSlider/>
 
       <Loginonpage/>   
+      <Userfeed/>
       
      
       {/* <Serviceinfo /> */}
