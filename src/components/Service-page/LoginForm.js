15290573566c590cfb092b75e011b41f814e7291
unknown <madhavsetia24@gmail.com>
2023-09-20 15:23:22 +0530

    Modified file added

--- src/components/Service-page/LoginForm.js
+++ src/components/Service-page/LoginForm.js
@@ -10,15 +10,15 @@ function LoginForm() {
         <h1 className='signup-heading'>Sign up and get clients today !</h1>
         <form className='form-elements'>
         <label for="fname" >Full Name</label>
-        <input className='input-tag' type="text" id="fname" name="fname" placeholder='Your Name'></input>
+        <input type="text" id="fname" name="fname" placeholder='Your Name'></input>
         <label for="email">Email</label>
-        <input className='input-tag' type="email" id="email" name="email" placeholder='yourname@gmail.com'></input>
+        <input type="email" id="email" name="email" placeholder='yourname@gmail.com'></input>
         <label for="phone">Phone number</label>
-        <input className='input-tag' type="tel" id="phone" placeholder='+917333399997' name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
+        <input type="tel" id="phone" placeholder='+917333399997' name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
         <label for="pwd">Password</label>
-        <input className='input-tag' type="password" id="pwd" name="pwd" placeholder='*******'></input>
+        <input type="password" id="pwd" name="pwd" placeholder='*******'></input>
         <label for="cpwd">Confirm Password</label>
-        <input className='input-tag' type="password" id="cpwd" name="cpwd" placeholder='*******'></input>
+        <input type="password" id="cpwd" name="cpwd" placeholder='*******'></input>
         </form>
         <div></div>
         <button className='signup-btn'>Sign Up</button>
--- src/components/Service-page/Tools.js
+++ src/components/Service-page/Tools.js
@@ -10,25 +10,25 @@ function Tools() {
             <li className='f'>
             <img src={icon} width={50} height={35}/>
               <div>
-              <h4 className='tools-features'>Analytics</h4>
+              <h4>Analytics</h4>
               <p className='tools-content'>A dashboard for showcasing your analytics.Monitor your growth ,Reach and track your ads and promotion</p>
-              <hr className='hr-tag'/>
+              <hr/>
               </div>
             </li>
             <li className='f'>
             <img src={icon} width={50} height={35}/>
             <div>
-              <h4 className='tools-features'>Referral</h4>
+              <h4>Referral</h4>
               <p className='tools-content'>Referral Program where we let others and you refer to other people.Increase your reach and get quality clients</p>
-              <hr className='hr-tag'/>
+              <hr/>
               </div>
             </li>
             <li className='f'>
             <img src={icon} width={50} height={35}/>
                 <div>
-              <h4 className='tools-features'>Gamified</h4>
+              <h4>Gamified</h4>
               <p className='tools-content'>Weekly dashboards that rank's according to your popularity,reach,ratings,reviews and that lead you generate</p>
-              <hr className='hr-tag'/>
+              <hr/>
               </div>
             </li>
             
