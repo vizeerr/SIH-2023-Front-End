195fa84558a49bba705067fd41dadc9a5e10d2f3
Rishav Raj <rishavraj1707music@gmail.com>
2023-09-23 18:58:15 +0530

    [feat] Add Profile page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-
 import "./App.css";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TranslateComponent from "./components/TranslateComponent";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="services" element={<Service />} />
-            <Route path="/translate" element={<TranslateComponent />}/>
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="services" element={<Service />} />
+          <Route exact path="profile" element={<Profile />} />
+          <Route path="/translate" element={<TranslateComponent />} />
+        </Routes>
+      </Router>
 
       {/* <Home /> */}
       {/* <Service /> */}
-   
     </>
   );
 }
